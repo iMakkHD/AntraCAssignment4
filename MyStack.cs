45dc5c0c4621329e911ad47e27601aec75ec06f0
Steven Mak <85149718+iMakkHD@users.noreply.github.com>
2022-03-28 06:12:54 -0500

    Add files via upload
    Only added partial solutions. Did not know how to add the rest.

---
+++ MyStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class MyStack<T>
+    {
+        int capacity;
+        T[] stack;
+        int top;
+
+        public MyStack(int cap)
+        {
+            capacity = cap;
+            stack = new T[capacity];
+        }
+        public void count(){
+
+        }
+
+        public int push(T item)
+        {
+            if(top == capacity - 1)
+            {
+                return -1;
+            }
+            else
+            {
+                top = top + 1;
+                stack[top] = item;
+            }
+            return 0;
+        }
+
+        public T pop()
+        {
+            T Removed;
+            T temp = default(T);
+            if (!(top <= 0))
+            {
+                Removed = stack[top];
+                top = top - 1;
+                return Removed;
+            }
+            return temp;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * 1. Generics address the issue of knowing what type will be used at run time.
+ * We cannot always know the type that will be used. Generics allow us to generalize the type
+ * and use whichever type when we run the code.
+ * 
+ * 2. List<string> sList = new List<string>()
+ * 
+ * 3. Two parameters
+ * 
+ * 4. True
+ * 
+ * 5. List.add(value);
+ * 
+ * 6. Remove() and Clear()
+ * 
+ * 7. <T>
+ * 
+ * 8. True
+ * 
+ * 9. True
+ * 
+ * 10. True
+ * 
+ * 
+ */
